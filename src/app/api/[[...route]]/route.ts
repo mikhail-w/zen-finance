a2c0aef8d0aa7a5d114b0d86d3de9768ad20d0ad
mikhail <mikhail.waddell@gmail.com>
2025-03-05 03:42:47 -0500

    test routes with hono middleware

---
+++ src/app/api/[[...route]]/route.ts
@@ -0,0 +1,38 @@
+import { z } from 'zod';
+import { Hono } from 'hono';
+import { handle } from 'hono/vercel';
+import { zValidator } from '@hono/zod-validator';
+import { clerkMiddleware, getAuth } from '@hono/clerk-auth';
+
+export const runtime = 'edge';
+
+const app = new Hono().basePath('/api');
+
+app
+  .get('/hello', c => {
+    return c.json({
+      message: 'Hello Next.js!',
+    });
+  })
+  .get('/hello/:test', c => {
+    return c.json({
+      message: 'Hello World',
+    });
+  })
+  .post(
+    '/',
+    zValidator(
+      'json',
+      z.object({
+        name: z.string(),
+        userId: z.number(),
+      })
+    ),
+    c => {
+      const { name, userId } = c.req.valid('json');
+      return c.json({});
+    }
+  );
+
+export const GET = handle(app);
+export const POST = handle(app);
