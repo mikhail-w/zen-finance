294b873a9e0193542f95db8dcef6f5b9c84f9a53
mikhail <mikhail.waddell@gmail.com>
2025-03-24 01:36:02 -0400

    create data cards and skeletons

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,40 +1,9 @@
-'use client'; // Required for client-side navigation
+import { DataGrid } from '@/components/data-grid';
 
-'use client';
-import { useGetAccounts } from '@/features/accounts/api/use-get-accounts';
-import { useRouter } from 'next/navigation';
-import { Button } from '@/components/ui/button'; // Import your custom Button component
-import { SignOutButton } from '@clerk/nextjs';
-import { useNewAccount } from '@/features/accounts/hooks/use-new-account';
-
-const Dashboard = () => {
-  const router = useRouter();
-  const accountsQuery = useGetAccounts();
-  const { onOpen } = useNewAccount();
-
+export default function DashboardPage() {
   return (
-    <div className="flex flex-col items-center justify-start h-screen mt-8 text-5xl font-bold">
-      <p className="mb-8">Dashboard</p>
-      <Button className="mb-8" onClick={onOpen}>
-        Add an Account
-      </Button>
-      <Button
-        variant="nav"
-        size="lg"
-        className="text-lg px-6 py-3"
-        onClick={() => router.push('/')} // Navigate back to the main page
-      >
-        Back to Home
-      </Button>
-      <div className="mt-20">
-        <SignOutButton>
-          <button className="px-4 py-2 bg-red-500 text-white rounded">
-            Sign Out
-          </button>
-        </SignOutButton>
-      </div>
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <DataGrid />
     </div>
   );
-};
-
-export default Dashboard;
+}
