4df63607f21ff3486a837c03b9a3ea05f4fa0394
mikhail <mikhail.waddell@gmail.com>
2025-05-15 19:49:57 -0400

    test

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -9,9 +9,9 @@ const DashboardLayout = ({ children }: Props) => {
   return (
     <>
       <DashHeader />
-      {/* <main className="px-4 sm:px-6 md:px-8 lg:px-14 max-w-screen-2xl mx-auto w-full"> */}
-      <main className="max-w-screen-2xl mx-auto w-full pb-10 pt-6 px-4 sm:px-6">
-        {children}
+      <main className="px-4 sm:px-6 md:px-8 lg:px-14 max-w-screen-2xl mx-auto w-full">
+      {/* <main className="max-w-screen-2xl mx-auto w-full pb-10 pt-6 px-4 sm:px-6">
+        {children} */}
       </main>
       ;
     </>
